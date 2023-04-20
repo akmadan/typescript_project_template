2d7d4989b528be6b90dccd5bcaf5db2d47ec0d30
Akshit Madan <akmadan1717@gmail.com>
2023-04-20 22:45:08 +0530

    added empty files

---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router = Router();
+// import expenseRouter from "../api/expense/routes/expense.route";
+// import userRouter from "../api/user/routes/user.routes";
+
+// router.use('/expense', expenseRouter);
+// router.use('/user', userRouter);
+
+export default router;
